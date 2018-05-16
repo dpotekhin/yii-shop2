c65501289de0c3ab11402fe92257297ac6107c04
Dmitry Potekhin <gackiep@gmail.com>
2018-05-16 18:59:29 +0300

    #19. Заказ - добавление таблиц для заказа в БД - генерация моделей таблиц заказа в GII - вид страницы заказа - форма заказа

--- views/cart/view.php
+++ views/cart/view.php
@@ -1,1 +1,69 @@
-Корзина
+
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+
+?>
+
+<div>
+
+    <h1>Корзина</h1>
+
+    <?php if( !empty($session['cart'])): ?>
+        <div class="table-responsive">
+            <table class="table table-hover table-stripped">
+                <thead>
+                <tr>
+                    <th>Фото</th>
+                    <th>Наименование</th>
+                    <th>Цена</th>
+                    <th>Кол-во</th>
+                    <th>Сумма</th>
+                    <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach($session['cart'] as $id => $item ): ?>
+                    <tr>
+                        <td><?= \yii\helpers\Html::img( '@web/images/products/'.$item['img'], ['alt' => $item['name'], 'height' => 50 ]) ?></td>
+                        <td><a href="<?= Url::to(['product/view', 'id' => $id ] ) ?>"><?= $item['name'] ?></a></td>
+                        <td><?= $item['price'] ?></td>
+                        <td><?= $item['qty'] ?></td>
+                        <td><?= $item['qty'] * $item['price'] ?></td>
+                        <td><span class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true" data-id="<?= $id ?>"></span></td>
+                    </tr>
+                <?php endforeach ?>
+
+                <tr>
+                    <td colspan="3">Итого:</td>
+                    <td><?= $session['cart.qty'] ?></td>
+                    <td  colspan="2" ></td>
+                </tr>
+
+                <tr>
+                    <td colspan="4">Сумма:</td>
+                    <td><?= $session['cart.sum'] ?></td>
+                    <td  colspan="1" ></td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+        <hr/>
+
+        <?php $form = ActiveForm::begin() ?>
+            <?= $form->field($order, 'name') ?>
+            <?= $form->field($order, 'email') ?>
+            <?= $form->field($order, 'phone') ?>
+            <?= $form->field($order, 'address') ?>
+            <?= Html::submitButton('Заказать', ['class' => 'btn btn-success']) ?>
+        <?php $form = ActiveForm::end() ?>
+
+    <?php else: ?>
+        <h3>Корзина пуста</h3>
+    <?php endif; ?>
+
+</div>
